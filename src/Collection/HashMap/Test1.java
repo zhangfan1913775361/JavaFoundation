dea0597e654d9e7c2e64bf6fcf7c6329053999eb
zhangfan <sa517486@mail.ustc.edu.cn>
2020-03-13 23:16:28 +0800

    Hashmap的test1

---
+++ src/Collection/HashMap/Test1.java
@@ -0,0 +1,15 @@
+package Collection.HashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test1 {
+    public static void main(String[] args) {
+        Map map = new HashMap();
+        map.put("lenovo",1300);
+        map.put("iPhone6",5300);
+        map.put("oppo R9s Plus","3500");
+        System.out.println(map);
+
+    }
+}
